43a17305c2deba58a4d5a9d62be6a025e7778927
YERKOPOMA20 <cocohenck@gmail.com>
2024-12-02 16:45:46 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+// JavaScript para animación de scroll suave
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
